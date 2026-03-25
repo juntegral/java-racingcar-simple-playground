105bf1de0de841186d99fd317fde9031178e184d
PARK JUNSEONG <jun0829park@gmail.com>
2026-03-26 00:45:40 +0900

    feat: 1단계 과제를 위한 자동차 객체 생성 및 이름 길이 검증 기능 구현
    - Car 클래스를 생성하였고. 이름 및 위치정보를 퍼블릭 메서드로 선언하였음.
    - 객체 생성시 위치 초기화하였음.
    - 이름이 5자를 초과할 경우 IllegalArgumentException을 throw하는 로직을 추가하였음(비강제성)
    - 인덴트 뎁스 1을 유지하기 위해서 따로 메서드를 분리함.

---
+++ src/main/java/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(final String name) {
+        validateNameLength(name);
+        this.name = name;
+        this.position = 0;
+    }
+
+    private void validateNameLength(final String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
