3003265aad7635d9fc7b8321bed53a500f15a478
PARK JUNSEONG <jun0829park@gmail.com>
2026-03-26 01:44:12 +0900

    feat: Cars 클래스가 있는 파일 생성 및 우승자 판별 기능 구현
    - N대의 Car 객체를 감싸는 Cars 클래스를 정의함
    - 가장 멀리 이동한 위치(highestPosition)를 찾아 우승자를 구하는 findWinners 로직을 추가함
    - for문 내부의 if문을 addIfWinner 메서드로 분리하여 인덴트 뎁스 요구사항 만족함

---
+++ src/main/java/Cars.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(final List<Car> cars) {
+        this.cars = new ArrayList<>(cars);
+    }
+
+    public List<Car> findWinners() {
+        int highestPosition = getHighestPosition();
+        List<Car> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            addIfWinner(winners, car, highestPosition);
+        }
+        return winners;
+    }
+
+    private void addIfWinner(final List<Car> winners, final Car car, final int highestPosition) {
+        if (car.isSamePosition(highestPosition)) {
+            winners.add(car);
+        }
+    }
+
+    private int getHighestPosition() {
+        int highest = 0;
+        for (Car car : cars) {
+            highest = Math.max(highest, car.getPosition());
+        }
+        return highest;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
