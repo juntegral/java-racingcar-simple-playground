e7b6a4b42fc491eef1acafefcd7506cd0a8ba7f6
PARK JUNSEONG <jun0829park@gmail.com>
2026-03-28 17:28:55 +0900

    feat: 메모리를 더 효율적으로 사용할 수 있도록 스태틱으로 수정하였습니다.

--- src/main/java/RacingController.java
+++ src/main/java/RacingController.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class RacingController {
     private static final Scanner scanner = new Scanner(System.in);
-    private final NumberGenerator numberGenerator = new RandomNumberGenerator();
+    private static final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
     public void run() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
