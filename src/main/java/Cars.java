03b36f361fda18996934746b97f08626bf23e533
PARK JUNSEONG <jun0829park@gmail.com>
2026-03-28 17:28:16 +0900

    feat: 리스트를 직접 조작하는 방식을 피하고, 순수하게 값을 조합하여 반환하도록 수정하였습니다.

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -9,27 +9,37 @@ public class Cars {
     }
 
     public List<Car> findWinners() {
-        int highestPosition = getHighestPosition();
-        List<Car> winners = new ArrayList<>();
+        Car maxCar = getMaxPositionCar();
+        return getSamePositionCars(maxCar);
+    }
 
+    private List<Car> getSamePositionCars(final Car maxCar) {
+        List<Car> winners = new ArrayList<>();
         for (Car car : cars) {
-            addIfWinner(winners, car, highestPosition);
+            addIfWinner(winners, car, maxCar);
         }
         return winners;
     }
 
-    private void addIfWinner(final List<Car> winners, final Car car, final int highestPosition) {
-        if (car.isSamePosition(highestPosition)) {
+    private void addIfWinner(final List<Car> winners, final Car car, final Car maxCar) {
+        if (car.isSamePosition(maxCar)) {
             winners.add(car);
         }
     }
 
-    private int getHighestPosition() {
-        int highest = 0;
+    private Car getMaxPositionCar() {
+        Car maxCar = cars.get(0);
         for (Car car : cars) {
-            highest = Math.max(highest, car.getPosition());
+            maxCar = compareAndGetMax(maxCar, car);
         }
-        return highest;
+        return maxCar;
+    }
+
+    private Car compareAndGetMax(final Car maxCar, final Car car) {
+        if (car.getPosition() > maxCar.getPosition()) {
+            return car;
+        }
+        return maxCar;
     }
 
     public List<Car> getCars() {
@@ -38,7 +48,7 @@ public class Cars {
 
     public void moveAll(final NumberGenerator numberGenerator) {
         for (Car car : cars) {
-            car.move(numberGenerator.generate()); // indent 1 유지
+            car.move(numberGenerator);
         }
     }
 }
