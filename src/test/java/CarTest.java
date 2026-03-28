35a52330eaee89ee3aaa292818efbf18546435da
PARK JUNSEONG <jun0829park@gmail.com>
2026-03-28 17:29:50 +0900

    feat: 단순 객체 생성이 아닌 이름의 경계값(5자, 6자)을 검증하는 방식으로 목적을 명확히 하고, 변경된 move 메서드에 맞추어 모킹된 생성자를 주입하도록 수정했습니다.

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -9,12 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class CarTest {
 
     @Test
-    @DisplayName("자동차 이름 길이 검증 테스트")
-    void 자동차_이름_길이_검증() {
+    @DisplayName("자동차 이름 경계값 검증 테스트")
+    void 자동차_이름_경계값_검증() {
         assertAll(
                 () -> assertThatCode(() -> new Car("pobi"))
                         .doesNotThrowAnyException(),
-                () -> assertThatThrownBy(() -> new Car("javajigi"))
+                () -> assertThatCode(() -> new Car("12345"))
+                        .doesNotThrowAnyException(),
+                () -> assertThatThrownBy(() -> new Car("123456"))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.")
         );
@@ -24,7 +26,10 @@ class CarTest {
     @DisplayName("무작위 값이 4 이상일 경우 자동차는 한 칸 전진한다.")
     void 자동차_전진_성공() {
         Car car = new Car("pobi");
-        car.move(4);
+        NumberGenerator alwaysMoveGenerator = () -> 4;
+
+        car.move(alwaysMoveGenerator);
+
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
@@ -32,7 +37,10 @@ class CarTest {
     @DisplayName("무작위 값이 3 이하일 경우 자동차는 정지한다.")
     void 자동차_정지_성공() {
         Car car = new Car("crong");
-        car.move(3);
+        NumberGenerator alwaysStopGenerator = () -> 3;
+
+        car.move(alwaysStopGenerator);
+
         assertThat(car.getPosition()).isEqualTo(0);
     }
 }
